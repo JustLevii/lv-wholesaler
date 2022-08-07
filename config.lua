d334df4fe7b75115e5508cfd54494c4a90a5d0f7
JustLevii <80844706+JustLevii@users.noreply.github.com>
2022-08-07 15:46:11 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,118 @@
+Config = {}
+Config.MoneyType = "bank"
+Config.Items = {
+    ["ILLEGAL"] = {
+        ["civtrophy"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489101165469796/civtrophy.png",
+            name = "Sivil Ödül",
+            itemName = "civtrophy",
+            value = 200,
+        },
+        ["laptop01"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489419580272710/laptop01.png",
+            name = "Laptop",
+            itemName = "laptop01",
+            value = 200,
+        },
+        ["maltesefalcon"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Heykel",
+            itemName = "maltesefalcon",
+            value = 200,
+        },
+        ["mictrophy"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Mikrofon Ödülü",
+            itemName = "mictrophy",
+            value = 200,
+        },
+        ["nokiaphone"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Nokia 3310",
+            itemName = "nokiaphone",
+            value = 200,
+        },
+        ["platinumrecord"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Plak",
+            itemName = "platinumrecord",
+            value = 200,
+        },
+        ["stolencoffee"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Kahve Makinesi",
+            itemName = "stolencoffee",
+            value = 200,
+        },
+        ["stolencomputer"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Bilgisayar",
+            itemName = "stolencomputer",
+            value = 200,
+        },
+        ["stolenmicrowave"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Mikrodalga",
+            itemName = "stolenmicrowave",
+            value = 200,
+        },
+        ["stolenmusic"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = " Ses Sistemi",
+            itemName = "stolenmusic",
+            value = 200,
+        },
+        ["stolenshoes"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Ayakkabı",
+            itemName = "stolenshoes",
+            value = 200,
+        },
+        ["stolentv"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Tv",
+            itemName = "stolentv",
+            value = 200,
+        },
+        ["walkman"] = {
+            img = "https://cdn.discordapp.com/attachments/975790406447022151/976489818827681822/maltesefalcon.png",
+            name = "Walkman",
+            itemName = "walkman",
+            value = 200,
+        },
+        dailyEarnings = 99999999,
+        unlimited = false,
+    },
+    ["ODUN"] = {
+        ["packaged_plank"] = {
+            img = "https://cdn.discordapp.com/attachments/959440648216404040/977604478108848278/packaged_plank.png",
+            name = "Odun",
+            itemName = "packaged_plank",
+            value = 20,
+        },
+        dailyEarnings = 99999999,
+        unlimited = false,
+    },
+    ["BALIK"] = {
+        ["goldchain"] = {
+            img = "https://cdn.discordapp.com/attachments/959440648216404040/977604617166790657/fish.png",
+            name = "Altın Kolye",
+            itemName = "goldchain",
+            value = 20,
+        },
+        ["diamond_ring"] = {
+            img = "https://cdn.discordapp.com/attachments/959440648216404040/977604617166790657/fish.png",
+            name = "Elmas Yüzük",
+            itemName = "diamond_ring",
+            value = 20,
+        },
+        ["rolex"] = {
+            img = "https://cdn.discordapp.com/attachments/959440648216404040/977604617166790657/fish.png",
+            name = "Rolex",
+            itemName = "rolex",
+            value = 20,
+        },
+        dailyEarnings = 99999999,
+        unlimited = true,
+    }
+}
